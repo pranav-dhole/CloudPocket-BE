4741889344f35b92e2ecb3f859ac36531c2b9c18
Bizzare <bizzareforyou@gmail.com>
2026-08-12 00:04:55 +0530

    modified folder routes for UI

--- routes/folderRoutes.js
+++ routes/folderRoutes.js
@@ -3,6 +3,7 @@ import { mkdir, rename, writeFile } from "fs/promises";
 import path, { join } from "path";
 import { isPathSafe, STORAGE_PATH } from "../utils/paths.js";
 import filesData from "../filesDB.json" with { type: "json" };
+import foldersData from "../foldersDB.json" with { type: "json" };
 
 const router = express.Router();
 
@@ -47,12 +48,24 @@ router.patch("/edit/:fileId", async (req, res) => {
     const fileData = filesData.find((file) => file.id === relativePath);
 
     const newFileName = req.body.newFileName;
-    if (newFileName === "/" || newFileName === `\\` || newFileName === "..") {
+    if (newFileName === fileData.fileName) {
       return res.status(403).json({ msg: "Filename denied" });
     }
 
     fileData.fileName = newFileName;
-    await writeFile("./filesDB.json", JSON.stringify(filesData));
+
+    const parentFolderData = foldersData.find(
+      (folder) => folder.id === fileData.parentFolderId,
+    );
+    const folderFileData = parentFolderData.files.find(
+      (file) => file.id === relativePath,
+    );
+    folderFileData.fileName = newFileName;
+
+    await Promise.all([
+      writeFile("./filesDB.json", JSON.stringify(filesData, null, 2)),
+      writeFile("./foldersDB.json", JSON.stringify(foldersData, null, 2)),
+    ]);
     res.status(200).json({ msg: "file renamed successfully" });
   } catch (err) {
     console.error(err);
@@ -60,6 +73,30 @@ router.patch("/edit/:fileId", async (req, res) => {
       return res.status(404).json({ msg: "Directory not found" });
     }
     res.status(500).json({ msg: "Error renaming directory" });
+  }
+});
+
+router.get("/:fileId", async (req, res) => {
+  try {
+    const { fileId } = req.params;
+
+    if (!fileId) {
+      const folderData = foldersData[0];
+      const files = folderData.files.map((folderFile) =>
+        filesData.find((file) => file.id === folderFile.id),
+      );
+
+      res.json({ ...folderData, files });
+    } else {
+      const folderData = foldersData.find((folder) => folder.id === fileId);
+      res.json(folderData);
+    }
+  } catch (err) {
+    console.error(err);
+    if (err.code === "ENOENT") {
+      return res.status(404).json({ msg: "Directory not found" });
+    }
+    res.status(500).json({ msg: "Error reading directory" });
   }
 });
 
