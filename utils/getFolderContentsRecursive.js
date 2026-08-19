1d08e5f4d1dc702b0af445c2f8ac21e97162d9fd
Bizzare <bizzareforyou@gmail.com>
2026-08-19 20:18:41 +0530

    recursive appraoch helper function for folder deletion

---
+++ utils/getFolderContentsRecursive.js
@@ -0,0 +1,17 @@
+export function getFolderContentsRecursive(targetFolderId, allFolders) {
+  const folder = allFolders.find((f) => f.id === targetFolderId);
+  if (!folder) {
+    return { folderIds: [], fileIds: [] };
+  }
+
+  let folderIds = [targetFolderId];
+  let fileIds = (folder.files || []).map((file) => file.id);
+
+  for (const childFolderId of folder.folders || []) {
+    const contents = getFolderContentsRecursive(childFolderId, allFolders);
+    folderIds = folderIds.concat(contents.folderIds);
+    fileIds = fileIds.concat(contents.fileIds);
+  }
+
+  return { folderIds, fileIds };
+}
