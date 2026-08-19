7d9180f354e806a1cfc3e7441d669f0093390cf3
Bizzare <bizzareforyou@gmail.com>
2026-08-19 20:17:46 +0530

    created a seperate DELETE method for folders using recursive approach

--- routes/folderRoutes.js
+++ routes/folderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { mkdir, rename, writeFile } from "fs/promises";
+import { mkdir, rename, rm, writeFile } from "fs/promises";
 import path, { join } from "path";
 import { isPathSafe, STORAGE_PATH } from "../utils/paths.js";
 import filesData from "../filesDB.json" with { type: "json" };
 import foldersData from "../foldersDB.json" with { type: "json" };
+import { getFolderContentsRecursive } from "../utils/getFolderContentsRecursive.js";
 
 const router = express.Router();
 
@@ -86,6 +87,70 @@ router.get("/{:folderId}", async (req, res) => {
       return res.status(404).json({ msg: "Directory not found" });
     }
     res.status(500).json({ msg: "Error reading directory" });
+  }
+});
+
+router.delete("/delete/:folderId", async (req, res) => {
+  try {
+    const { folderId } = req.params;
+    const targetFolder = foldersData.find((folder) => folder.id === folderId);
+
+    const { folderIds, fileIds } = getFolderContentsRecursive(
+      folderId,
+      foldersData,
+    );
+
+    for (const fileId of fileIds) {
+      const fileData = filesData.find((f) => f.id === fileId);
+      if (fileData) {
+        try {
+          const filePath = `${STORAGE_PATH}/${fileData.id}${fileData.fileExtension}`;
+          await rm(filePath, { force: true, recursive: true });
+        } catch (err) {
+          console.warn(err);
+          res.status(500).json({
+            message:
+              "something went wrong while deleting files from file id's data",
+          });
+        }
+      }
+    }
+
+    for (let i = filesData.length - 1; i >= 0; i--) {
+      if (fileIds.includes(filesData[i].id)) {
+        filesData.splice(i, 1);
+      }
+    }
+    for (let i = foldersData.length - 1; i >= 0; i--) {
+      if (folderIds.includes(foldersData[i].id)) {
+        foldersData.splice(i, 1);
+      }
+    }
+
+    if (targetFolder.parentFolderId) {
+      const parentFolder = foldersData.find(
+        (folder) => folder.id === targetFolder.parentFolderId,
+      );
+      if (parentFolder) {
+        parentFolder.folders = parentFolder.folders.filter(
+          (dirId) => dirId !== folderId,
+        );
+      }
+    }
+
+    await Promise.all([
+      writeFile("./filesDB.json", JSON.stringify(filesData, null, 2)),
+      writeFile("./foldersDB.json", JSON.stringify(foldersData, null, 2)),
+    ]);
+
+    res
+      .status(200)
+      .json({ message: "Folder and contents deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ message: "Something went wrong while deleting folder" });
   }
 });
 
