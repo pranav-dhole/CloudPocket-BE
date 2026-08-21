109cacf09f0baea67e4ae5da9f18734094ce8dc7
Bizzare <bizzareforyou@gmail.com>
2026-08-21 17:42:59 +0530

    Error handling : existing folder routes

--- routes/folderRoutes.js
+++ routes/folderRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { mkdir, rename, rm, writeFile } from "fs/promises";
 import path, { join } from "path";
-import { isPathSafe, STORAGE_PATH } from "../utils/paths.js";
+import { STORAGE_PATH } from "../utils/paths.js";
 import filesData from "../filesDB.json" with { type: "json" };
 import foldersData from "../foldersDB.json" with { type: "json" };
 import { getFolderContentsRecursive } from "../utils/getFolderContentsRecursive.js";
@@ -11,13 +11,14 @@ const router = express.Router();
 // handling new folder creation logic
 router.post("/create/{:parentFolderId}", async (req, res) => {
   try {
-    const parentFolderId = req.params.parentFolderId || foldersData[0].id;
-    const folderName = req.headers.foldername;
+    const parentFolderId = req.params.parentFolderId || foldersData?.[0]?.id;
+    if (!parentFolderId)
+      return res
+        .status(400)
+        .json({ message: "No default parent folder available" });
 
-    if (!folderName) {
-      return res.status(400).json({ msg: "Folder name parameter is missing" });
-    }
-
+    const folderName =
+      (req.headers.foldername && req.headers.foldername.trim()) || "New Folder";
     const id = crypto.randomUUID();
     const parentFolder = foldersData.find(
       (folder) => folder.id === parentFolderId,
@@ -31,14 +32,16 @@ router.post("/create/{:parentFolderId}", async (req, res) => {
       folders: [],
     };
 
-    parentFolder.folders.push(id);
+    parentFolder.folders?.push(id);
     foldersData.push(newFolderData);
     await writeFile("./foldersDB.json", JSON.stringify(foldersData, null, 2));
 
-    res.status(200).json({ msg: "Folder created successfully", newFolderData });
+    return res.status(201).json({ message: "Folder created successfully" });
   } catch (err) {
     console.error(err);
-    res.status(404).json({ msg: "Error while creating an folder", err: err });
+    return res
+      .status(500)
+      .json({ message: "Error occured while creating an folder" });
   }
 });
 
@@ -47,22 +50,25 @@ router.patch("/edit/:folderId", async (req, res) => {
   try {
     const folderId = req.params.folderId;
     const folderData = foldersData.find((folder) => folder.id === folderId);
+    if (!folderData || folderData === -1)
+      return res.status(404).json({ message: "Folder doesnt exist" });
 
     const newFolderName = req.body.newFolderName;
     if (newFolderName === folderData.folderName) {
-      return res.status(403).json({ msg: "Folder name denied" });
+      return res.status(403).json({ message: "Folder name denied" });
+    } else if (!newFolderName || !newFolderName.trim()) {
+      return res.status(400).json({ message: "Folder name is required" });
     }
 
     folderData.folderName = newFolderName;
 
     await writeFile("./foldersDB.json", JSON.stringify(foldersData, null, 2));
-    res.status(200).json({ msg: "folder renamed successfully" });
+    return res.status(200).json({ message: "Folder renamed successfully" });
   } catch (err) {
     console.error(err);
-    if (err.code === "ENOENT") {
-      return res.status(404).json({ msg: "Directory not found" });
-    }
-    res.status(500).json({ msg: "Error renaming directory" });
+    return res
+      .status(500)
+      .json({ message: "Error occured while renaming folder" });
   }
 });
 
@@ -72,21 +78,38 @@ router.get("/{:folderId}", async (req, res) => {
     const folderData = folderId
       ? foldersData.find((folder) => folder.id === folderId)
       : foldersData[0];
-    const files = folderData.files.map((folderFile) =>
-      filesData.find((file) => file.id === folderFile.id),
+    if (!folderData || folderData === -1)
+      return res.status(404).json({ message: "Folder doesnt exist" });
+
+    const resolvedFiles = folderData.files?.map((ff) =>
+      filesData.find((file) => file.id === ff.id),
     );
 
-    const folders = folderData.folders
-      .map((folderId) => foldersData.find((folder) => folder.id === folderId))
-      .map(({ id, folderName }) => ({ id, folderName }));
+    const resolvedFolders = folderData.folders.map((id) =>
+      foldersData.find((folder) => folder.id === id),
+    );
 
-    res.json({ ...folderData, files, folders });
+    const hasResolvedFiles =
+      !resolvedFiles || resolvedFiles.includes(undefined);
+    const hasResolvedFolders =
+      !resolvedFolders || resolvedFolders.includes(undefined);
+
+    if (hasResolvedFiles || hasResolvedFolders)
+      return res
+        .status(404)
+        .json({ message: "One or more files OR folders dont exist" });
+
+    const files = resolvedFiles;
+    const folders = resolvedFolders.map(({ id, folderName }) => {
+      (id, folderName);
+    });
+
+    return res.status(200).json({ ...folderData, files, folders });
   } catch (err) {
     console.error(err);
-    if (err.code === "ENOENT") {
-      return res.status(404).json({ msg: "Directory not found" });
-    }
-    res.status(500).json({ msg: "Error reading directory" });
+    return res
+      .status(500)
+      .json({ message: "Error occured while reading folder" });
   }
 });
 
@@ -94,6 +117,8 @@ router.delete("/delete/:folderId", async (req, res) => {
   try {
     const { folderId } = req.params;
     const targetFolder = foldersData.find((folder) => folder.id === folderId);
+    if (!targetFolder || targetFolder === -1)
+      return res.status(404).json({ message: "Folder doesnt exist" });
 
     const { folderIds, fileIds } = getFolderContentsRecursive(
       folderId,
@@ -108,21 +133,20 @@ router.delete("/delete/:folderId", async (req, res) => {
           await rm(filePath, { force: true, recursive: true });
         } catch (err) {
           console.warn(err);
-          res.status(500).json({
-            message:
-              "something went wrong while deleting files from file id's data",
+          return res.status(500).json({
+            message: "Error occured while deleting files from folder deletion",
           });
         }
       }
     }
 
     for (let i = filesData.length - 1; i >= 0; i--) {
-      if (fileIds.includes(filesData[i].id)) {
+      if (fileIds.includes(filesData[i]?.id)) {
         filesData.splice(i, 1);
       }
     }
     for (let i = foldersData.length - 1; i >= 0; i--) {
-      if (folderIds.includes(foldersData[i].id)) {
+      if (folderIds.includes(foldersData[i]?.id)) {
         foldersData.splice(i, 1);
       }
     }
@@ -143,14 +167,14 @@ router.delete("/delete/:folderId", async (req, res) => {
       writeFile("./foldersDB.json", JSON.stringify(foldersData, null, 2)),
     ]);
 
-    res
+    return res
       .status(200)
       .json({ message: "Folder and contents deleted successfully" });
   } catch (err) {
     console.error(err);
-    res
+    return res
       .status(500)
-      .json({ message: "Something went wrong while deleting folder" });
+      .json({ message: "Error occured while deleting folder" });
   }
 });
 
