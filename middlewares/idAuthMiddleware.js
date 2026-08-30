4cd2b1c64cd33f55739fbe39d54255c6cceb9ffa
Bizzare <bizzareforyou@gmail.com>
2026-08-30 16:38:57 +0530

    Add uuid auth middleware

---
+++ middlewares/idAuthMiddleware.js
@@ -0,0 +1,8 @@
+export default function idAuth(req, res, next, id) {
+  const uuidRegex =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+  if (!uuidRegex.test(id)) {
+    return res.status(400).json({ message: "Invalid ID" });
+  }
+  next();
+}
