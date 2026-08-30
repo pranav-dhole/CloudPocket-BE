0342a46a93e7fc802218d6833b94ec7b881cced4
Bizzare <bizzareforyou@gmail.com>
2026-08-30 16:41:55 +0530

    Add uuid auth middleware

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -23,6 +23,12 @@ router.post("/register", async (req, res) => {
   const userId = crypto.randomUUID();
   const folderId = crypto.randomUUID();
 
+  const uuidRegex =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+  if (!uuidRegex.test(userId) || !uuidRegex.test(folderId)) {
+    return res.status(400).json({ message: "Invalid ID" });
+  }
+
   foldersData.push({
     id: folderId,
     name: `root-${email}`,
