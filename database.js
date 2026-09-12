251eadc0f31b0cb24a48606cd5c4089235a352ea
Bizzare <bizzareforyou@gmail.com>
2026-09-12 22:00:50 +0530

    Add mongodb connect function which stops/starts on node's server start/stop

--- database.js
+++ database.js
@@ -2,23 +2,17 @@ import { MongoClient } from "mongodb";
 import { readFile } from "fs/promises";
 
 const client = new MongoClient("mongodb://localhost:27017/");
-await client.connect();
 
-const db = client.db("cloudpockets");
-const usersCollection = db.collection("users");
-const foldersCollection = db.collection("folders");
-const filesCollection = db.collection("files");
+export async function connectDB() {
+  await client.connect();
+  console.log("Connected to database");
 
-const filesRawdata = await readFile("./filesDB.json", "utf-8");
-const foldersRawdata = await readFile("./foldersDB.json", "utf-8");
-const usersRawdata = await readFile("./usersDB.json", "utf-8");
+  const db = client.db("cloudpockets");
+  return db;
+}
 
-const filesData = JSON.parse(filesRawdata);
-const foldersData = JSON.parse(foldersRawdata);
-const usersData = JSON.parse(usersRawdata);
-
-usersCollection.insertMany(usersData);
-foldersCollection.insertMany(foldersData);
-filesCollection.insertMany(filesData);
-
-client.close();
+process.on("SIGINT", async () => {
+  await client.close();
+  console.log("Disconnected from database");
+  process.exit(0);
+});
