ec0656c23ae39d847ee5597392e76284345a4566
Bizzare <bizzareforyou@gmail.com>
2026-09-12 22:01:56 +0530

    Modified search from local json to mongodb

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,9 +1,16 @@
-import usersData from "../usersDB.json" with { type: "json" };
+import { ObjectId } from "mongodb";
 
-export function checkAuth(req, res, next) {
+export async function checkAuth(req, res, next) {
   const { uid } = req.cookies;
-  const loggedInUser = usersData.find((user) => user.id === uid);
-  if (!uid || !loggedInUser) {
+  if (!uid) {
+    return res.status(401).json({ message: "Not an valid user" });
+  }
+  const db = req.db;
+  const loggedInUser = await db
+    .collection("users")
+    .findOne({ _id: new ObjectId(uid) });
+
+  if (!loggedInUser) {
     return res.status(401).json({ message: "Not an valid user" });
   }
   req.user = loggedInUser;
