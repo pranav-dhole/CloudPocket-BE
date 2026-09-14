9f62f30880b73994214a6135bd1e434d59f1536c
Bizzare <bizzareforyou@gmail.com>
2026-09-14 15:46:27 +0530

    Modified fileRoutes to perform CRUD operations on mongodb instead of local json file

--- routes/filesRoutes.js
+++ routes/filesRoutes.js
@@ -1,50 +1,43 @@
 import express from "express";
 import { createWriteStream } from "fs";
-import { mkdir, readdir, rename, rm, stat, writeFile } from "fs/promises";
+import { rm, unlink } from "fs/promises";
 import { pipeline } from "stream/promises";
 import path, { join } from "path";
 import { STORAGE_PATH } from "../utils/paths.js";
-import filesData from "../filesDB.json" with { type: "json" };
-import foldersData from "../foldersDB.json" with { type: "json" };
-import { extension } from "mime-types";
-import crypto from "crypto";
 import idAuth from "../middlewares/idAuthMiddleware.js";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-router.param("parentFolderId", idAuth);
-router.param("fileId", idAuth);
+// router.param("parentFolderId", idAuth);
+// router.param("fileId", idAuth);
 
-// file deleting logic
+// deleting the file using given fileId
 router.delete("/:fileId", async (req, res) => {
   try {
     const { fileId } = req.params;
-    const fileIdx = filesData.findIndex((file) => file.id === fileId);
-
-    if (fileIdx < 0 || fileIdx === undefined || fileIdx === null)
+    const fileObjectId = new ObjectId(fileId);
+    const db = req.db;
+    const fileData = await db
+      .collection("files")
+      .findOne({ _id: fileObjectId });
+    if (!fileData) {
       return res.status(404).json({ message: "File doesnt exist" });
-    const fileData = filesData[fileIdx];
-    const filePath = path.join(STORAGE_PATH, fileId + fileData.fileExtension);
+    }
+    const filePath = path.join(STORAGE_PATH, fileId + fileData?.fileExtension);
 
-    filesData.splice(fileIdx, 1);
-    const parentFolderData = foldersData.find(
-      (folder) => folder.id === fileData.parentFolderId,
-    );
+    const parentFolderData = await db
+      .collection("folders")
+      .find({ _id: fileData.parentFolderId });
 
     if (!parentFolderData)
       return res.status(404).json({ message: "Parent folder doesnt exist" });
-    parentFolderData.files = parentFolderData.files.filter(
-      (id) => id !== fileId,
-    );
 
-    await Promise.all([
-      writeFile("./filesDB.json", JSON.stringify(filesData, null, 2)),
-      writeFile("./foldersDB.json", JSON.stringify(foldersData, null, 2)),
-    ]);
-
+    await db.collection("files").deleteOne({ _id: fileObjectId });
     await rm(filePath, {
       recursive: true,
     });
+
     return res.status(200).json({ message: "File deleted successfully" });
   } catch (err) {
     console.error(err);
@@ -52,22 +45,23 @@ router.delete("/:fileId", async (req, res) => {
   }
 });
 
-// getting files from path
-router.get("/:fileId", (req, res) => {
+// getting files from given fileId
+router.get("/:fileId", async (req, res) => {
   const { fileId } = req.params;
-  const fileData = filesData.find((file) => file.id === fileId);
-  const parentFolder = foldersData.find(
-    (folder) => folder.id === fileData.parentFolderId,
-  );
+  const fileObjectId = new ObjectId(fileId);
+  const db = req.db;
+  const fileData = await db.collection("files").findOne({ _id: fileObjectId });
+  if (!fileData) {
+    return res.status(404).json({ message: "File not found!" });
+  }
 
-  if (parentFolder.userId !== req.user.id) {
+  const parentFolder = await db
+    .collection("folders")
+    .findOne({ _id: new ObjectId(fileData.parentFolderId) });
+  if (parentFolder.userId.toString() !== req.user._id.toString()) {
     return res
       .status(401)
       .json({ message: "You are not authorized to view this file" });
-  }
-
-  if (!fileData) {
-    return res.status(404).json({ message: "File not found!" });
   }
 
   const filePath = `${STORAGE_PATH}/${fileId}${fileData.fileExtension}`;
@@ -83,15 +77,20 @@ router.get("/:fileId", (req, res) => {
   });
 });
 
-// handling the posted files from the client
+// handling the posted files from the client using its parentFolderId
 router.post("/{:parentFolderId}", async (req, res) => {
+  let createdFileId = null;
+  let filePath = null;
   try {
     const parentFolderId = req.params.parentFolderId || req.user.rootFolderId;
-    const parentFolder = foldersData.find(
-      (folder) => folder.id === parentFolderId,
-    );
-
-    if (parentFolder.userId !== req.user.id) {
+    const parentFolderObjectId = new ObjectId(parentFolderId);
+    const db = req.db;
+    const parentFolder = await db
+      .collection("folders")
+      .findOne({ _id: parentFolderObjectId });
+    if (!parentFolder)
+      return res.status(404).json({ message: "Parent folder doesnt exist" });
+    if (parentFolder.userId.toString() !== req.user._id.toString()) {
       return res
         .status(401)
         .json({ message: "You are not authorized to upload this file" });
@@ -102,76 +101,64 @@ router.post("/{:parentFolderId}", async (req, res) => {
       return res.status(400).json({ message: "Filename is required" });
 
     const decodedFileName = decodeURIComponent(fileName);
-    const id = crypto.randomUUID();
     const sanitizedBaseName = path.basename(decodedFileName);
     const fileExtension = path.extname(sanitizedBaseName);
-    const fullFileName = id + fileExtension;
 
-    const writeStream = createWriteStream(
-      path.join(STORAGE_PATH, fullFileName),
-    );
-    await pipeline(req, writeStream);
-
-    const newRecord = {
-      id,
+    const newFileRecord = await db.collection("files").insertOne({
       fileExtension,
       fileName: decodedFileName,
-      parentFolderId,
-    };
+      parentFolderId: parentFolderObjectId,
+    });
+    createdFileId = newFileRecord.insertedId;
+    const fullFileName = createdFileId.toString() + fileExtension;
+    filePath = path.join(STORAGE_PATH, fullFileName);
+    const writeStream = createWriteStream(filePath);
 
-    filesData.push(newRecord);
+    req.on("aborted", () => {
+      writeStream.destroy();
+    });
+    await pipeline(req, writeStream);
 
-    const fileParentFolder = foldersData.find(
-      (folder) => folder.id === parentFolderId,
-    );
-
-    if (!fileParentFolder)
-      return res.status(404).json({ message: "Parent folder doesnt exist" });
-    fileParentFolder.files.push(id);
-
-    await Promise.all([
-      writeFile("./filesDB.json", JSON.stringify(filesData, null, 2)),
-      writeFile("./foldersDB.json", JSON.stringify(foldersData, null, 2)),
-    ]);
     return res.status(201).json({ message: "File created successfully" });
   } catch (err) {
     console.error(err);
-    return res
-      .status(500)
-      .json({ message: "Error occured while uploading file" });
+    if (filePath) await unlink(filePath).catch(() => null);
+    if (createdFileId) {
+      await req.db
+        .collection("files")
+        .deleteOne({ _id: createdFileId })
+        .catch(console.error);
+    }
+    if (!res.headersSent) {
+      return res
+        .status(500)
+        .json({ message: "Error occured while uploading file" });
+    }
   }
 });
 
-// handling file rename stuff
+// handling file rename using given fileId
 router.patch("/:fileId", async (req, res) => {
   try {
     const { fileId } = req.params;
-    const fileData = filesData.find((file) => file.id === fileId);
-    const parentFolder = foldersData.find(
-      (folder) => folder.id === fileData.parentFolderId,
-    );
+    const fileObjectId = new ObjectId(fileId);
+    const db = req.db;
+    const fileData = await db
+      .collection("files")
+      .findOne({ _id: fileObjectId });
+    if (!fileData)
+      return res.status(404).json({ message: "File doesnt exist" });
 
-    if (parentFolder.userId !== req.user.id) {
+    const parentFolder = await db
+      .collection("folders")
+      .findOne({ _id: new ObjectId(fileData.parentFolderId) });
+    if (!parentFolder)
+      return res.status(404).json({ message: "Parent folder doesnt exist" });
+    if (parentFolder.userId.toString() !== req.user._id.toString()) {
       return res
         .status(401)
         .json({ message: "You are authorizeed to edit this file" });
     }
-
-    if (!fileData || fileData === -1)
-      return res.status(404).json({ message: "File doesnt exist" });
-
-    const parentFolderData = foldersData.find(
-      (folder) => folder.id === fileData.parentFolderId,
-    );
-    if (!parentFolderData || parentFolderData === -1)
-      return res.status(404).json({ message: "Parent folder doesnt exist" });
-
-    const folderData = filesData.find((file) => file.id === fileId);
-
-    if (!folderData || folderData === -1)
-      return res
-        .status(404)
-        .json({ message: "File doesnt exist in parent folder" });
 
     const newFileName = req.body.newFileName;
     if (newFileName === fileData.fileName) {
@@ -180,13 +167,9 @@ router.patch("/:fileId", async (req, res) => {
       return res.status(400).json({ message: "File name is required" });
     }
 
-    fileData.fileName = newFileName;
-    folderData.fileName = newFileName;
-
-    await Promise.all([
-      writeFile("./filesDB.json", JSON.stringify(filesData, null, 2)),
-      writeFile("./foldersDB.json", JSON.stringify(foldersData, null, 2)),
-    ]);
+    await db
+      .collection("files")
+      .updateOne({ _id: fileObjectId }, { $set: { fileName: newFileName } });
 
     return res.status(200).json({ message: "File renamed successfully" });
   } catch (err) {
