d4c8cb30e492193630c3e450baa5be8fecfcf02c
Bizzare <bizzareforyou@gmail.com>
2026-09-14 15:47:47 +0530

    Modified under the need for mongodb files/folders searching

--- utils/getFolderContentsRecursive.js
+++ utils/getFolderContentsRecursive.js
@@ -1,17 +1,39 @@
-export function getFolderContentsRecursive(targetFolderId, allFolders) {
-  const folder = allFolders.find((f) => f.id === targetFolderId);
-  if (!folder) {
-    return { folderIds: [], fileIds: [] };
+import { ObjectId } from "mongodb";
+
+// getting all the files(id & fileExtension) and folders(id) info of an given targetFolderId recursively
+export async function getFolderContentsRecursive(db, targetFolderId, userId) {
+  let allFolderIds = [new ObjectId(targetFolderId)];
+  let allFileIds = [];
+
+  const files = await db
+    .collection("files")
+    .find({
+      parentFolderId: targetFolderId,
+    })
+    .project({ _id: 1, fileExtension: 1 })
+    .toArray();
+
+  allFileIds.push(...files);
+
+  const subFolders = await db
+    .collection("folders")
+    .find({
+      parentFolderId: targetFolderId,
+      userId: userId,
+    })
+    .project({ _id: 1 })
+    .toArray();
+
+  for (const subFolder of subFolders) {
+    const contents = await getFolderContentsRecursive(
+      db,
+      subFolder._id,
+      userId,
+    );
+
+    allFolderIds.push(...contents.folderIds);
+    allFileIds.push(...contents.fileIds);
   }
 
-  let folderIds = [targetFolderId];
-  let fileIds = (folder.files || []).map((id) => id);
-
-  for (const childFolderId of folder.folders || []) {
-    const contents = getFolderContentsRecursive(childFolderId, allFolders);
-    folderIds = folderIds.concat(contents.folderIds);
-    fileIds = fileIds.concat(contents.fileIds);
-  }
-
-  return { folderIds, fileIds };
+  return { folderIds: allFolderIds, fileIds: allFileIds };
 }
